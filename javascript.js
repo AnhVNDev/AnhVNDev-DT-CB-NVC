bcc8e95a3498eaf83a8d3475a058abaa18fc2c4b
anhvn <anhvn@anhvn.com>
2025-07-28 17:31:08 +0700

    update

---
+++ javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#owl-carousel-banner").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 1 },
+      1000: { items: 1 },
+    },
+  });
+});
